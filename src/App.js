0ed63519639a09b79da983bd9eb017942519ce65
Charles DeLong <c.thecheesedelong@gmail.com>
2017-03-29 15:58:43 -0700

    built JSON file to be filled in later

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import MyAlbums from './MyAlbums'
+import Album from './Album'
+import Photo from './Photo'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 export default React.createClass({
--- src/MyAlbums.js
+++ src/MyAlbums.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default React.createClass({
   render() {
     return(
-    	
+    	<div>
+    		<h1>My Albums</h1>
+    		<div>
+    			
+    		</div>
+    	</div>
     )
   }
 )}

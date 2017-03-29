bd4f3dd310b6dcb82c73ded8de7526b188613d3c
Charles DeLong <c.thecheesedelong@gmail.com>
2017-03-29 15:23:42 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+export default React.createClass({
+  render() {
+    return(
+      <Router>
+        <div>
+          /* 
+          My albums component 
+          Individual Album component
+          Photo component
+          */
+        </div>
+      </Router>
+    )
+  }
+)}

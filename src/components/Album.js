7c06cb76144a12629f410bc27d0fa77f8d8ee453
Charles DeLong <c.thecheesedelong@gmail.com>
2017-04-28 16:49:56 -0700

    last commit

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,46 +2,64 @@ import React from 'react'
 import Albums from '../assets/Pictures.json'
 import { Link } from 'react-router-dom'
  
-export default React.createClass({
-  handleClick() {
-    return {
-
+class SingleAlbum extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      name: Albums.gallery.filter(album=>{
+        return Number(album.id) === this.props.match.params.albumID
+      })[0].name,
+      photos: Albums.photos.filter(photo=>{
+        return Number(photo.albumid) === this.props.match.params.albumID
+      })
     }
-  },
-   getInitialState() {
-    return {
-      photos: Albums.gallery.filter(gallery=>{
-        return Number(gallery.id) === Number(this.props.match.params.albumId)
-              
-
-      })[0]
-    }
-  },
+  }
 
   render() {
+    console.log(this.state)
     return(
-    	<div>
-    		<aside>
-    			<nav>
-    				<ul>
-            {Albums.photos.map(pic=>(
-    					<Link to="">
-    					<li onClick={pic.handleClick}>{pic.state.gallery.photos.name}</li>
-    					</Link>
-              ))}
-    				</ul>
-    			</nav>
-    		</aside>
-    		<main className='mainAlbums'>
-        {this.props.match.params.albumId.photos.map(image=>(
-          <Link key={image.id} to={'/Photo/' + image.id}>
-          <div className='thumbBlock'>
-            <img className='thumbnail' src={image.url} alt={image.name} />
-          </div>
-          </Link>
+       <div>
+        <h1>{this.state.name}</h1>
+        <div className='grid'>
+          {this.state.photos.map(photo=>(
+            <Link key={photo.id} to={'/Album/' + photo.id}>
+              <div className='galleryBlock'>
+                <img src={photo.url} alt={photo.id} />
+                <p>{photo.name}</p>
+              </div>
+            </Link>
           ))}
-    		</main>
-    	</div>
+        </div>
+      </div>
     )
   }
-})
+}
+
+export default SingleAlbum
+
+
+
+
+// <div>
+//         <aside>
+//           <h1>{this.state.name}</h1>
+//           <nav>
+//             <ul>
+//             {this.state.albums.map(pic=>(
+//               <Link key={gallery.id} to={'/Album/' + gallery.id}>
+//               <li onClick={pic.handleClick}>{pic.state.gallery.photos.name}</li>
+//               </Link>
+//               ))}
+//             </ul>
+//           </nav>
+//         </aside>
+//         <main className='mainAlbums'>
+//         {this.props.match.params.albumId.photos.map(image=>(
+//           <Link key={image.id} to={'/Photo/' + image.id}>
+//           <div className='thumbBlock'>
+//             <img className='thumbnail' src={image.url} alt={image.name} />
+//           </div>
+//           </Link>
+//           ))}
+//         </main>
+//       </div>
--- src/components/MyAlbums.js
+++ src/components/MyAlbums.js
@@ -2,25 +2,33 @@ import React from 'react'
 import Albums from '../assets/Pictures.json'
 import { Link } from 'react-router-dom'
 
+class MyAlbums extends React.Component {
+   constructor() {
+    super()
+    this.state = {
+      albums: Albums.gallery
+    }
+  }
 
-export default React.createClass({
+
   render() {
     return(
     	<div>
     		<h1>My Albums</h1>
     		<div className='mainBit'>
-    			{Albums.gallery.map(gallery=>(
-    			<Link key={gallery.id} to={'/Album/' + gallery.id}>
-    			<div className='galleryBlock'>
-    				<img src={gallery.coverphoto} alt={gallery.id} />
-    				<p>{gallery.name}</p>
-    			</div>
-    			</Link>
+    			{this.state.albums.map(gallery=>(
+      			<Link key={gallery.id} to={'/Album/' + gallery.id}>
+        			<div className='galleryBlock'>
+        				<img src={gallery.coverphoto} alt={gallery.name} />
+        				<p>{gallery.name}</p>
+        			</div>
+      			</Link>
     			))}
     		</div>
     	</div>
     )
   }
-})
+}
+export default MyAlbums
 
 					
